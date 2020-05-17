3326c66f212a051cb081823df3c15e0731285b91
boshipingtian <1619794359@qq.com>
2020-05-17 19:56:41 +0800

    apple demo

---
+++ Apple/js/style.main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $("#know").click(function () {
+        $("#know_con").slideToggle("slow");
+    });
+});
+$(document).ready(function () {
+    $("#serve").click(function () {
+        $("#serve_con").slideToggle(400);
+    });
+});
+$(document).ready(function () {
+    $("#account").click(function () {
+        $("#account_con").slideToggle(400);
+    });
+});
+$(document).ready(function () {
+    $("#shop").click(function () {
+        $("#shop_con").slideToggle("slow");
+    });
+});
+$(document).ready(function () {
+    $("#business").click(function () {
+        $("#business_con").slideToggle(400);
+    });
+});
+$(document).ready(function () {
+    $("#edu").click(function () {
+        $("#edu_con").slideToggle(400);
+    });
+});
+$(document).ready(function () {
+    $("#value").click(function () {
+        $("#value_con").slideToggle(400);
+    });
+});
+$(document).ready(function () {
+    $("#about").click(function () {
+        $("#about_con").slideToggle("slow");
+    });
+});
